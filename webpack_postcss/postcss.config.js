1dcfdefb4e8d644fa0089e2eee10c788471f2917
YANH216 <65383837+YANH216@users.noreply.github.com>
2021-12-13 19:09:06 +0800

    Delete webpack_postcss directory

--- webpack_postcss/postcss.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    plugins: [
-      require('postcss-preset-env') //预设(postcss-preset-env)包含autoprefixer
-    ]
-  }
--- webpack_postcss/src/js/api.js
+++
@@ -1,8 +0,0 @@
-const getInfo = () => {
-    return {
-        name : 'yh',
-        age : 21
-    }
-}
-
-module.exports = getInfo
